718fd3b866a3f945cdaac6d9ab94641f4c49e4bb
Narlson Costa <naldrums@gmail.com>
2022-10-29 01:21:15 -0300

    initial commit
    initial commit

---
+++ Old_West_Bank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Old_West_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
