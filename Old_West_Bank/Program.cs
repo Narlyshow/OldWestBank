ef0db611efd858672016486feb5a8122f2aac241
Narlson Costa <naldrums@gmail.com>
2022-10-29 02:57:53 -0300

    Tela de boas vindas

--- Old_West_Bank/Program.cs
+++ Old_West_Bank/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Old_West_Bank
 {
@@ -6,7 +7,10 @@ namespace Old_West_Bank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+     
+
+            
         }
     }
 }
---
+++ Old_West_Bank/UI/TelaBoasVindas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Old_West_Bank.UI
+{
+    class TelaBoasVindas
+    {
+    
+        public void TelaApresentacao()
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            string arquivo = Path.Combine(path + @"\Txt_Tela", "TELA_INICIAL.txt");
+
+            if (File.Exists(arquivo))
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(arquivo))
+                    {
+                        String linha;
+                        // Lê linha por linha até o final do arquivo
+                        while ((linha = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(linha);
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine(" O arquivo inicial não foi localizado !");
+            }
+            
+        }
+              
+    
+    }
+}
