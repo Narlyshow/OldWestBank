05ea46c9e2a5f6fd42db3473896d139d1e777f82
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-10-31 22:40:00 -0300

    Superclasse Abstrata criada
    Criei a superclasse abstrata Account para futuramente serem criadas as subclasses dela.

---
+++ Old_West_Bank/BackEnd/Entities/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Old_West_Bank.BackEnd.Entities
+{
+    abstract class Account
+    {
+        public int Number { get; set; }
+        public double Balance { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public string HolderName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime AccountDate { get; set; }
+
+
+
+        public abstract void Withdraw(double amount);
+
+        public abstract void Deposit(double amount);
+    }
+}
--- Old_West_Bank/UI/TelaCadastro.cs
+++ Old_West_Bank/UI/TelaCadastro.cs
@@ -13,11 +13,11 @@ namespace Old_West_Bank.UI
             Console.WriteLine();
             sb.AppendLine("* Bem vindo ao Cadastro Old West! *");
             sb.AppendLine("Está pronto para ser o cowboy mais rico desse velho oeste?");
-            sb.AppendLine("Antes de começarmos com o seu cadastro, peço que por favor escolha qual conta deseja criar.");
+            sb.AppendLine("Espero que sim. Antes de começarmos com o seu cadastro, peço que por favor escolha qual conta deseja criar.");
+            sb.AppendLine();
             sb.AppendLine("Se deseja criar uma Conta Poupança digite ----> 1");
             sb.AppendLine("Se deseja criar uma Conta Empresarial digite ----> 2");
             sb.AppendLine("Ou se deseja criar uma Conta Corrente digite ----> 3");
-            sb.AppendLine();
             sb.AppendLine("Se ainda não estiver pronto para fazer parte desse Velho Oeste, volte mais tarde ----> 0");
 
             return sb.ToString();
